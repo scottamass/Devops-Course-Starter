e05bfd0fca5fc7c71de5519589d96437ff8651a3
scottamass <scot.atkinson@live.co.uk>
2021-12-31 15:14:23 +0000

    started adding create new items

--- todo_app/app.py
+++ todo_app/app.py
@@ -1,4 +1,7 @@
-from flask import Flask
+from sys import ps1
+from flask import Flask ,render_template, sessions, request
+from todo_app.data.session_items import add_item, get_items
+
 
 from todo_app.flask_config import Config
 
@@ -8,4 +11,14 @@ app.config.from_object(Config())
 
 @app.route('/')
 def index():
-    return 'Hello World!'
+    
+
+
+    return render_template('index.html',items = get_items())
+
+@app.route('/submit', methords =['POST'])
+def submit():
+    if request.methord =='POST':
+        pass
+
+
