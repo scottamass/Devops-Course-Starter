75c7b12b45bd1258c9d514ff130dac0dbba404ab
scottamass <scot.atkinson@live.co.uk>
2022-01-03 17:15:11 +0000

    can now tasks to list

--- todo_app/app.py
+++ todo_app/app.py
@@ -1,5 +1,6 @@
-from sys import ps1
-from flask import Flask ,render_template, sessions, request
+
+from flask import Flask ,render_template, sessions, request,redirect, url_for
+
 from todo_app.data.session_items import add_item, get_items
 
 
@@ -16,9 +17,11 @@ def index():
 
     return render_template('index.html',items = get_items())
 
-@app.route('/submit', methords =['POST'])
+@app.route('/submit', methods=['POST'] )
 def submit():
-    if request.methord =='POST':
-        pass
+    if request.method =='POST':
+        title=request.form.get('title')
+        add_item(title)
+        return redirect(url_for('index'))
 
 
