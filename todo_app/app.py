58d729181df5031f098038f5096184df1609067c
scottamass <scot.atkinson@live.co.uk>
2022-01-13 16:11:47 +0000

    i have added funactionality to the button

--- todo_app/app.py
+++ todo_app/app.py
@@ -1,7 +1,7 @@
 
 from flask import Flask ,render_template, sessions, request,redirect, url_for
 
-from todo_app.data.session_items import add_item, get_items ,get_item
+from todo_app.data.session_items import add_item, get_items ,get_item,save_item
 
 
 from todo_app.flask_config import Config
@@ -23,12 +23,14 @@ def submit():
         title=request.form.get('title')
         add_item(title)
         return redirect(url_for('index'))
-#
 
-@app.route("/complete<id>", methods=['PUT'])
+
+@app.route("/complete/<id>", methods=['POST'])
 def complete(id):
     
-        get_item(id).status = "done"
+        item=get_item(id)
+        item['status'] ="done"
+        save_item(item)
         
         
         return redirect(url_for('index'))
